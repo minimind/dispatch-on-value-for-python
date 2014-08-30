5df0bba6d2313a5a60d2417c088708c8c8cd7fd0
Ian Macinnes <ian.macinnes@gmail.com>
2014-08-30 22:59:40 +0100

    Added any_x functionality

--- pymultipledispatchonvalue/pymultidispatchonvalue.py
+++ pymultipledispatchonvalue/pymultidispatchonvalue.py
@@ -1,7 +1,38 @@
 import itertools
 
 
+class AnyValue:
+    def __init__(self, num):
+        self.num = num
+
+any_a = AnyValue(1)
+any_b = AnyValue(2)
+any_c = AnyValue(3)
+any_d = AnyValue(4)
+
+
+def dispatch(fn, stream, pattern):
+    (matched, matched_stream) = match(stream, pattern)
+    if matched:
+        fn(matched_stream)
+        return True
+    else:
+        return False
+
+
 def match(stream, pattern):
+    return real_match(stream, pattern, {})
+
+
+def real_match(stream, pattern, context):
+    # Maybe it's any_x?
+    if isinstance(pattern, AnyValue):
+        if pattern.num in context:
+            return context[pattern.num] == stream, stream
+        else:
+            context[pattern.num] = stream
+            return True, stream
+
     # We'll assume it's callable
     try:
         return pattern(stream), stream
@@ -12,57 +43,52 @@ def match(stream, pattern):
 
         # OK, we'll assume it's a dictionary
         try:
-            raise AttributeError()  # return compare_dictionaries(stream, pattern)
+            return compare_dictionaries(stream, pattern, context)
 
         except AttributeError:
             # Maybe a string or a list?
             try:
+                # I hate to add an isinstance here but I can't see 
+                # how to lose it
                 if isinstance(stream, basestring):
-                    return compare_primitives(stream, pattern)
+                    return compare_primitives(stream, pattern, context)
                 else:
-                    return compare_lists(stream, pattern)
+                    return compare_lists(stream, pattern, context)
 
             except TypeError:
                 # Have to assume primitives
-                return compare_primitives(stream, pattern)
+                return compare_primitives(stream, pattern, context)
 
 
-def compare_primitives(stream, pattern):
+def compare_primitives(stream, pattern, context):
     if type(stream) == type(pattern) and stream == pattern:
         return True, stream
     else:
         return False, []
 
 
-def compare_lists(stream, pattern):
+def compare_lists(stream, pattern, context):
     # We compare each item in the list. If they all match, then we have
     # a match.
     if not len(stream) == len(pattern):
         return False, []
 
     for s, p in itertools.izip(stream, pattern):
-        (matched, matched_stream) = match(s, p)
+        (matched, matched_stream) = real_match(s, p, context)
         if not matched:
             return False, []
 
     return True, stream
 
 
-'''def compare_dictionaries(stream, pattern):
+def compare_dictionaries(stream, pattern, context):
     for k, v in pattern.iteritems():
         if not k in stream:
             return False, []
 
-        v2 = stream[k]
-        if type(v) in primitive_types and type(v) == type(v2):
-            if not v == v2:
-                return False, []
+        s = stream[k]
+        (matched, matched_stream) = real_match(s, v, context)
+        if not matched:
+            return False, []
 
-        elif type(v) is dict and type(v2) is dict:
-            return compare_dictionaries(v, v2), stream
-
-        elif type(v) is list and type(v2) is list:
-            return compare_lists(v, v2), stream
-
-    return True, stream'''
-
+    return True, stream
--- pymultipledispatchonvalue/test/test_simple.py
+++ pymultipledispatchonvalue/test/test_simple.py
@@ -2,7 +2,7 @@ import unittest
 import pymultidispatchonvalue
 
 
-class TestSequenceFunctions(unittest.TestCase):
+class TestBasicMatching(unittest.TestCase):
 
     '''def test_shuffle(self):
         ran_fn1 = False
@@ -47,12 +47,14 @@ class TestSequenceFunctions(unittest.TestCase):
         pattern = '3km'
         (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
         assert matched
+        assert stream == stream_found
 
     def test_list1(self):
         stream = [4]
         pattern = [4]
         (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
         assert matched
+        assert stream == stream_found
 
     def test_list2(self):
         stream = [4]
@@ -65,6 +67,7 @@ class TestSequenceFunctions(unittest.TestCase):
         pattern = [4, 5]
         (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
         assert matched
+        assert stream == stream_found
 
     def test_list4(self):
         stream = [4, 5]
@@ -77,6 +80,7 @@ class TestSequenceFunctions(unittest.TestCase):
         pattern = [4, 5, [6, 7]]
         (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
         assert matched
+        assert stream == stream_found
 
     def test_list6(self):
         stream = [4, 5, [6, 7]]
@@ -89,6 +93,7 @@ class TestSequenceFunctions(unittest.TestCase):
         pattern = (1,2)
         (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
         assert matched
+        assert stream == stream_found
 
     def test_tuple2(self):
         stream = (1,2)
@@ -107,12 +112,14 @@ class TestSequenceFunctions(unittest.TestCase):
         pattern = lambda x: x == 1
         (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
         assert matched
+        assert stream == stream_found
 
     def test_lambda2(self):
         stream = 3
         pattern = lambda x: 1 < x < 5
         (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
         assert matched
+        assert stream == stream_found
 
     def test_lambda3(self):
         stream = 7
@@ -125,6 +132,7 @@ class TestSequenceFunctions(unittest.TestCase):
         pattern = [1, 2, lambda x: x == 3]
         (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
         assert matched
+        assert stream == stream_found
 
     def test_lambda5(self):
         stream = [1, 2, 4]
@@ -137,10 +145,143 @@ class TestSequenceFunctions(unittest.TestCase):
         pattern = [1, 2, [lambda x: x == 4, 5]]
         (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
         assert matched
+        assert stream == stream_found
 
     def test_lambda7(self):
         stream = [1, 2, [4, 5]]
         pattern = [1, 2, [lambda x: x == 4, 4]]
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert not matched
+
+    def test_dict1(self):
+        stream = {'one': 1}
+        pattern = {'one': 1}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_dict2(self):
+        stream = {'one': 1, 'two': 2}
+        pattern = {'one': 1, 'two': 2}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_dict3(self):
+        stream = {'one': 1, 'two': 3}
+        pattern = {'one': 1, 'two': 2}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert not matched
+
+    def test_dict4(self):
+        stream = {'one': 1, 'two': 2}
+        pattern = {'one': 1}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_dict5(self):
+        stream = {'one': 1}
+        pattern = {'one': 1, 'two': 2}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert not matched
+
+    def test_dict6(self):
+        stream = {'one': 1, 'list': [1, 2]}
+        pattern = {'one': 1, 'list': [1, 2]}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_dict7(self):
+        stream = {'one': 1, 'list': [1, 2]}
+        pattern = {'one': 1, 'list': [1, 3]}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert not matched
+
+    def test_dict8(self):
+        stream = {'one': 1, 'num': 6}
+        pattern = {'one': 1, 'num': lambda x: 3 < x < 9}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_dict9(self):
+        stream = {'one': 1, 'num': 10}
+        pattern = {'one': 1, 'num': lambda x: 3 < x < 9}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert not matched
+
+    def test_any1(self):
+        stream = 1
+        pattern = pymultidispatchonvalue.any_a
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_any2(self):
+        stream = 2
+        pattern = pymultidispatchonvalue.any_a
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_any3(self):
+        stream = [1, 2]
+        pattern = [pymultidispatchonvalue.any_a, 2]
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_any4(self):
+        stream = [1, 2, 1]
+        pattern = [pymultidispatchonvalue.any_a, 2,
+                   pymultidispatchonvalue.any_a]
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_any5(self):
+        stream = [1, 2, 2]
+        pattern = [pymultidispatchonvalue.any_a, 2,
+                   pymultidispatchonvalue.any_a]
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert not matched
+
+    def test_any6(self):
+        stream = [1, 2, 3, [1, 4, 5]]
+        pattern = [pymultidispatchonvalue.any_a, 2, 3,
+                   [pymultidispatchonvalue.any_a, 4, 5]]
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_any7(self):
+        stream = [1, 2, 3, [2, 4, 5]]
+        pattern = [pymultidispatchonvalue.any_a, 2, 3,
+                   [pymultidispatchonvalue.any_a, 4, 5]]
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert not matched
+
+    def test_any8(self):
+        stream = {'a': 1, 'b': 2, 'c': {'d': 2, 'e': 3}}
+        pattern = {'a': 1, 'b': pymultidispatchonvalue.any_b,
+                   'c': {'e': 3, 'd': pymultidispatchonvalue.any_b}}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert matched
+        assert stream == stream_found
+
+    def test_any9(self):
+        stream = {'a': 1, 'b': 2, 'c': {'d': 3, 'e': 3}}
+        pattern = {'a': 1, 'b': pymultidispatchonvalue.any_b,
+                   'c': {'e': 3, 'd': pymultidispatchonvalue.any_b}}
+        (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
+        assert not matched
+
+    def test_any10(self):
+        stream = {'a': 1, 'c': {'d': 2, 'e': 3}}
+        pattern = {'a': 1, 'b': pymultidispatchonvalue.any_b,
+                   'c': {'e': 3, 'd': pymultidispatchonvalue.any_b}}
         (matched, stream_found) = pymultidispatchonvalue.match(stream, pattern)
         assert not matched
 
