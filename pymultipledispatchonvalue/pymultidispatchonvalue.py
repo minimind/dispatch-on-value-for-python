bfae05b0e14478796a2026938e05ce3c3e234659
Ian Macinnes <ian.macinnes@gmail.com>
2014-08-30 21:15:28 +0100

    Now works with lists and primitives

--- pymultipledispatchonvalue/pymultidispatchonvalue.py
+++ pymultipledispatchonvalue/pymultidispatchonvalue.py
@@ -2,25 +2,44 @@ primitive_types = {int, float, str, bool}
 
 
 def match(stream, fn1, d, pattern):
-    if type(stream) in primitive_types and type(stream) == type(pattern):
-            if not stream == pattern:
-                return False, []
-
-    if type(pattern) is dict:
-        if not type(stream) is dict:
-            return False, []
-
-        (matched, tail) = compare_dictionaries(stream, pattern)
-        if not matched:
-            return False, []
+    if type(stream) in primitive_types:
+        return compare_primitive(stream, pattern)
 
     elif type(pattern) is list:
-        if not type(stream) is list:
-            return False, []
+        return compare_lists(stream, pattern)
 
-        (matched, tail) = compare_lists(stream, pattern)
-        if not matched:
-            return False, []
+
+def compare_primitive(stream, pattern):
+    assert type(stream) in primitive_types
+
+    if type(stream) == type(pattern) and stream == pattern:
+        return True
+    else:
+        return False
+
+
+def compare_lists(stream, pattern):
+    # We compare each item in the list. If they all match, then we have
+    # a match.
+    if not len(stream) == len(pattern):
+        return False, []
+
+    for s, p in zip(stream, pattern):
+        if type(p) in primitive_types:
+            matched = compare_primitive(s, p)
+            if not matched:
+                return False
+
+        elif type(p) is list:
+            matched = compare_lists(s, p)
+            if not matched:
+                return False
+
+    return True
+
+
+
+
 
 
 def compare_dictionaries(stream, pattern):
@@ -41,20 +60,3 @@ def compare_dictionaries(stream, pattern):
 
     return True, stream
 
-
-def compare_lists(stream, pattern):
-    if not len(stream) == len(pattern):
-        return False, []
-
-    for v, v2 in zip(pattern, stream):
-        if type(v) in primitive_types and type(v) == type(v2):
-            if not v == v2:
-                return False, []
-
-        elif type(v) is dict and type(v2) is dict:
-            return compare_dictionaries(v, v2)
-
-        elif type(v) is list and type(v2) is list:
-            return compare_lists(v, v2)
-
-    return True, stream
--- pymultipledispatchonvalue/test/test_simple.py
+++ pymultipledispatchonvalue/test/test_simple.py
@@ -1,9 +1,10 @@
 import unittest
 import pymultidispatchonvalue
 
+
 class TestSequenceFunctions(unittest.TestCase):
 
-    def test_shuffle(self):
+    '''def test_shuffle(self):
         ran_fn1 = False
 
         def fn1(li, a):
@@ -20,7 +21,69 @@ class TestSequenceFunctions(unittest.TestCase):
         )
 
         assert matched
-        assert ran_fn1
+        assert ran_fn1'''
+
+    def test_primitive1(self):
+        stream = 3
+        pattern = 3
+        matched = pymultidispatchonvalue.match(stream, None, None, pattern)
+        assert matched
+
+    def test_primitive2(self):
+        stream = 3
+        pattern = 4
+        matched = pymultidispatchonvalue.match(stream, None, None, pattern)
+        assert not matched
+
+    def test_primitive3(self):
+        stream = 3
+        pattern = '3'
+        matched = pymultidispatchonvalue.match(stream, None, None, pattern)
+        assert not matched
+
+    def test_primitive4(self):
+        stream = '3km'
+        pattern = '3km'
+        matched = pymultidispatchonvalue.match(stream, None, None, pattern)
+        assert matched
+
+    def test_list1(self):
+        stream = [4]
+        pattern = [4]
+        matched = pymultidispatchonvalue.match(stream, None, None, pattern)
+        assert matched
+
+    def test_list2(self):
+        stream = [4]
+        pattern = [5]
+        matched = pymultidispatchonvalue.match(stream, None, None, pattern)
+        assert not matched
+
+    def test_list3(self):
+        stream = [4, 5]
+        pattern = [4, 5]
+        matched = pymultidispatchonvalue.match(stream, None, None, pattern)
+        assert matched
+
+    def test_list4(self):
+        stream = [4, 5]
+        pattern = [4, 6]
+        matched = pymultidispatchonvalue.match(stream, None, None, pattern)
+        assert not matched
+
+    def test_list5(self):
+        stream = [4, 5, [6, 7]]
+        pattern = [4, 5, [6, 7]]
+        matched = pymultidispatchonvalue.match(stream, None, None, pattern)
+        assert matched
+
+    def test_list6(self):
+        stream = [4, 5, [6, 7]]
+        pattern = [4, 5, [6, 8]]
+        matched = pymultidispatchonvalue.match(stream, None, None, pattern)
+        assert not matched
+
+
 
 if __name__ == '__main__':
     unittest.main()
